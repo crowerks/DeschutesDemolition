873a9cf933cb8e50a45a8feff032fb9b15e7a2e0
crowerks ws2 <crowerks-ws2@crowerkss-iMac-2.local>
2017-06-02 15:21:28 -0700

    added method for highlighting row

--- controller.js
+++ controller.js
@@ -9,23 +9,23 @@ app.controller('mainController', ['$scope', '$log', 'getJSON', function($scope, 
     getJSON.data("1jzf554WLhpPimIPS2-YK2nPEKB0USgGHRr8IHsnMXlw")
         .then(function(responce) {
             $scope.rxMale = responce.data.feed.entry;
-            console.log(responce);
         
         })
         .catch(function(e) {
             $log.error(e);
         })
-//    getJSON.data("1jzf554WLhpPimIPS2-YK2nPEKB0USgGHRr8IHsnMXlw")
-//        .then(function(responce) {
-//            $scope.rxMale = responce.data.feed.entry;
-//            console.log(responce);
-//        
-//        })
-//        .catch(function(e) {
-//            $log.error(e);
-//        })
-    
-    
+    getJSON.data("1BGw28ur6SLm40SwDy-MohGHLf4E449cZ7Le9Mj6tAMQ")
+        .then(function(responce) {
+            $scope.rxFemale = responce.data.feed.entry;
+        
+        })
+        .catch(function(e) {
+            $log.error(e);
+        })
+   
+    $scope.setActive = function(tableRow) {
+        $scope.selectedRow = tableRow;
+    }
 }])
 
 
